a61b425c0fd59d1f28355ae16b5f84395af59e15
samanthafranks03 <rdu5qu@virginia.edu>
2025-04-01 14:07:52 +0200

    Adjusted 5+ aspects of the webpage

--- src/App.js
+++ src/App.js
@@ -78,7 +78,7 @@ export default function Game() {
     }
     return (
       <li key={move}>
-        <button onClick={() => jumpTo(move)}>{description}</button>
+        <button onClick={() => jumpTo(move)} className="description">{description}</button>
       </li>
     );
   });
